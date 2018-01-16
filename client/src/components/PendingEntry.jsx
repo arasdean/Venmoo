28423f2e1ceff07fa0b8b196e86573590e674f5c
Jake Luton <jake.luton92@gmail.com>
2018-01-15 17:28:12 -0800

    Change title, removes updatebalance from request. Bug fix.

--- client/src/components/PendingEntry.jsx
+++ client/src/components/PendingEntry.jsx
@@ -3,20 +3,6 @@ import axios from 'axios';
 const moment = require('moment');
 
 
-// const PendingEntry = (props) => {
-//   const id = props.entry.transaction_id;
-//   const accept = () => axios.post(`/transaction/accept/${id}-approved`);
-//   const decline = () => axios.post(`/transaction/accept/${id}-declined`);
-//   return (
-//     <li className="pendingItem">
-//       {props.entry.receiver_name} is requesting {props.entry.amount}
-//       <button onClick={accept}>agree to pay</button>
-//       <button onClick={decline}>decline request</button>
-//     </li>
-//   );
-// };
-
-
 class PendingEntry extends React.Component {
   constructor(props) {
     super(props);
@@ -37,7 +23,6 @@ class PendingEntry extends React.Component {
     const id = this.state.entry.transaction_id;
     const accept = () => {
       axios.post(`/transaction/accept/${id}-approved`);
-      this.hide();
       this.updateState();
     };
     const decline = () => {
